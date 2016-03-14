cfb74680ed2a026732eecb3f8ba0c80c3f281827
Andi Dittrich <andi@andidittrich.de>
2016-03-14 13:39:17 +0000

    initial release

---
+++ CONTRIBUTE.md
@@ -0,0 +1,10 @@
+
+Contribution
+------------
+
+Gulp-PrettyError is OpenSource and managed on [GitHub](https://github.com/AndiDittrich/gulp-prettyerror) - if you like, you're welcome to contribute!
+To simplify the release and quality control process, please follow these remarks:
+
+### Notices ###
+* Related software packages are updated by the maintainer
+* If you form a concept of larger project changes, please [discuss](https://github.com/AndiDittrich/gulp-prettyerror/issues) them with the contributors **before** implementing
---
+++ gulp-prettyerror.js
@@ -0,0 +1,32 @@
+var _gutil = require('gulp-util');
+var _gplumber = require('gulp-plumber');
+
+var PrettyError = (function(customErrorFormat){
+
+    // custom error format function provided ?
+    if (typeof customErrorFormat != 'undefined'){
+
+        // proxy
+        return _gplumber(function(error){
+            customErrorFormat.apply(this, [error, _gutil]);
+        });
+
+    }else{
+        // default appearance
+        return _gplumber(function(error){
+            // add indentation
+            var msg = error.codeFrame.replace(/\n/g, '\n    ');
+
+            _gutil.log('|- ' + _gutil.colors.bgRed.bold('Build Error in ' + error.plugin));
+            _gutil.log('|- ' + _gutil.colors.bgRed.bold(error.message));
+            _gutil.log('|- ' + _gutil.colors.bgRed('>>>'));
+            _gutil.log('|\n    ' + msg + '\n           |');
+            _gutil.log('|- ' + _gutil.colors.bgRed('<<<'));
+        });
+    }
+
+    // terminate pipe processing
+    this.emit('end');
+});
+
+module.exports = PrettyError;
